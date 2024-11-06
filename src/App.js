39cf79c97981f97513c2105d03cbee287079ebc7
stampHo <inho8024s@gmail.com>
2024-11-06 17:35:12 +0900

    1106_01

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div
+      className="App"
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "1000px",
+        fontSize: "100px",
+      }}
+    >
+      Hello World
+    </div>
+  );
+}
+
+export default App;
